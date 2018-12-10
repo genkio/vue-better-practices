08d9ec7296dca864d1278f58bdce58ef460d67ff
slashbit <5327840+slashbit@users.noreply.github.com>
2018-12-11 00:40:43 +0900

    chore: create item store module with mock data

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import category from './modules/category'
+import item from './modules/item'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   strict: true,
   modules: {
-    category
+    category,
+    item
   }
 })
--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -1,4 +1,20 @@
-const getDefaultState = () => {
+const state = getDefaultState()
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
+
+function getDefaultState () {
   return {
     categories: [
       {
@@ -43,20 +59,4 @@ const getDefaultState = () => {
       }
     ]
   }
-}
-
-const state = getDefaultState()
-
-const getters = {}
-
-const actions = {}
-
-const mutations = {}
-
-export default {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations
 }
