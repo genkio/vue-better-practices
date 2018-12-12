ab82a567dd638190cbeda8cadd5fd0fd06279f9a
slashbit <5327840+slashbit@users.noreply.github.com>
2018-12-12 16:23:19 +0900

    feature: create sell button

---
+++ src/components/common/SellButton.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div class="wrapper">
+    <span class="text">出品</span>
+    <span class="icon">
+      <i class="fas fa-camera"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SellButton',
+  props: {
+    onClick: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/main.scss';
+
+.wrapper {
+  position: relative;
+  width: 8rem;
+  height: 8rem;
+  line-height: 8rem;
+  border-radius: 50%;
+  color: $white;
+  text-align: center;
+  background: $orange;
+  cursor: pointer;
+
+  .icon {
+    font-size: 3rem;
+    position: relative;
+    top: 1.75rem;
+  }
+  .text {
+    font-size: 1.25rem;
+    display: inline-block;
+    position: absolute;
+    top: -1.75rem;
+    left: 2.75rem;
+  }
+}
+</style>
--- src/pages/Timeline.vue
+++ src/pages/Timeline.vue
@@ -2,6 +2,10 @@
   <div>
     <timeline-header :categories="categories"></timeline-header>
     <item-card-list :items="items"></item-card-list>
+    <sell-button
+      class="sell-button"
+      :on-click="onSell">
+    </sell-button>
   </div>
 </template>
 
@@ -10,19 +14,30 @@ import { mapState } from 'vuex'
 
 import TimelineHeader from '../components/header/TimelineHeader'
 import ItemCardList from '../components/item/ItemCardList'
+import SellButton from '../components/common/SellButton'
 
 export default {
   name: 'Timeline',
   components: {
     TimelineHeader,
     ItemCardList,
+    SellButton
   },
   computed: {
     ...mapState('category', ['categories']),
     ...mapState('item', ['items'])
+  },
+  methods: {
+    onSell () {}
   }
 }
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+.sell-button {
+  position: fixed;
+  bottom: -1.25rem;
+  right: -1.25rem;
+  overflow: hidden;
+}
 </style>
